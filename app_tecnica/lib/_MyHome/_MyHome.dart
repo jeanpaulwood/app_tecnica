b9aa1f1bbbfc1591966347378dc393025b997e59
jeanpaulwood <jeanpaulwood08@gmail.com>
2020-10-21 08:59:18 -0300

    2º Retirado a váriavel '_connectionsStatus
    Váriavel não estava sem uso dentro da aplicação e por isso foi removida.

--- app_tecnica/lib/_MyHome/_MyHome.dart
+++ app_tecnica/lib/_MyHome/_MyHome.dart
@@ -17,22 +17,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   // Variáveis
   final Connectivity _connectivity = Connectivity();
   final _formKey = GlobalKey<FormState>();
-  bool _isOnline  = true;
+  bool _isOnline = true;
   TextEditingController _login = TextEditingController();
   TextEditingController _password = TextEditingController();
-  String _connectionStatus = 'Unknown';
+
   StreamSubscription<ConnectivityResult> _connectivitySubscription;
   String url1 = 'https://jsonplaceholder.typicode.com/users';
-  String url2 = 'http://sc9.portalunitrac.com/scriptcase/app/UNITRACGR/apptecnicos_webservice_login/apptecnicos_webservice_login.php';
+  String url2 =
+      'http://sc9.portalunitrac.com/scriptcase/app/UNITRACGR/apptecnicos_webservice_login/apptecnicos_webservice_login.php';
 
-  AlertDialog _showAlertDialog(BuildContext context, String _errorTitle){
+  AlertDialog _showAlertDialog(BuildContext context, String _errorTitle) {
     // Configurar btn
     Widget okButton = FlatButton(
-      child: Text('OK',style: TextStyle(fontSize: 15.00, color: Colors.black),),
+      child: Text(
+        'OK',
+        style: TextStyle(fontSize: 15.00, color: Colors.black),
+      ),
       onPressed: () {
         Navigator.pop(context);
       },
@@ -40,49 +43,49 @@ class _MyHomePageState extends State<MyHomePage> {
     // Configurar o AlertDialog
     AlertDialog alert = AlertDialog(
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10.0))
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      title: Text(
+        'ERRO:',
       ),
-      title: Text('ERRO:',),
-      content: Text(_errorTitle,),
-      actions: <Widget>[
-        okButton
-      ],
+      content: Text(
+        _errorTitle,
+      ),
+      actions: <Widget>[okButton],
     );
     // Exibe o AlertDialog
     showDialog(
         context: context,
-        builder: (BuildContext context){
+        builder: (BuildContext context) {
           return alert;
-        }
-    );
+        });
     return AlertDialog();
   }
 
   // Futures
   Future<List<api_login>> _getUser(String login, String senha) async {
-    try{
+    try {
       List<api_login> listUser = List();
-      final response = await http.post(
-          url2,
-          body:{'login':login,'senha':senha} );
-      if(response.statusCode == 200){
+      final response =
+          await http.post(url2, body: {'login': login, 'senha': senha});
+      if (response.statusCode == 200) {
         try {
           var decodeJson = jsonDecode(response.body);
           decodeJson.forEach((item) => listUser.add(api_login.fromJson(item)));
         } catch (ex) {
           print(response.body);
-          print('Não foi possível decodificar o json. '+ex.toString());
+          print('Não foi possível decodificar o json. ' + ex.toString());
         }
         return listUser;
-      }else{
+      } else {
         print('Erro ao carregar lista 1.');
         return null;
       }
-    } catch (ex){
-      print('Erro ao carregar lista 2.'+ex.toString());
+    } catch (ex) {
+      print('Erro ao carregar lista 2.' + ex.toString());
       return null;
     }
   }
+
   Future<void> initConnectivity() async {
     ConnectivityResult result;
     // Platform messages may fail, so we use a try/catch PlatformException.
@@ -98,6 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return _updateConnectionStatus(result);
   }
+
   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
     switch (result) {
       case ConnectivityResult.wifi:
@@ -108,9 +112,6 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
       case ConnectivityResult.none:
         _isOnline = false;
-        break;
-      default:
-        setState(() => _connectionStatus = 'Failed to get connectivity.');
         break;
     }
   }
@@ -130,46 +131,44 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   // Widgets
-  Widget container_(){
-    return
-      Container(
-        width: 280.00,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            textFieldLogin_(),
-            SizedBox(height: 10),
-            textFieldPassaword_(),
-            raisedButton_(),
-          ],
-        ),
-      );
+  Widget container_() {
+    return Container(
+      width: 280.00,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          textFieldLogin_(),
+          SizedBox(height: 10),
+          textFieldPassaword_(),
+          raisedButton_(),
+        ],
+      ),
+    );
   }
 
-  Widget textFieldLogin_(){
-    return
-      TextFormField(
+  Widget textFieldLogin_() {
+    return TextFormField(
       controller: _login,
       cursorColor: Colors.yellowAccent,
       style: TextStyle(
-        //backgroundColor: Colors.black12,
-          color: Colors.yellowAccent
-      ),
+          //backgroundColor: Colors.black12,
+          color: Colors.yellowAccent),
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
           filled: true,
           fillColor: Colors.black12,
-
           labelText: 'Login',
           //helperText: 'E-mail do usuário',
           border: new OutlineInputBorder(
             //borderRadius: new BorderRadius.circular(25.0),
             borderSide: new BorderSide(color: Colors.yellowAccent),
           ),
-          icon: Icon(Icons.email, color: Colors.yellowAccent,)
-      ),
-      validator: (value){
-        if(value.isEmpty){
+          icon: Icon(
+            Icons.email,
+            color: Colors.yellowAccent,
+          )),
+      validator: (value) {
+        if (value.isEmpty) {
           return 'Preencha o login';
         }
         return null;
@@ -177,75 +176,70 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget textFieldPassaword_(){
-    return
-      TextFormField(
-        controller: _password,
-        cursorColor: Colors.yellowAccent,
-        style: TextStyle(
+  Widget textFieldPassaword_() {
+    return TextFormField(
+      controller: _password,
+      cursorColor: Colors.yellowAccent,
+      style: TextStyle(
           //backgroundColor: Colors.black12,
-            color: Colors.yellowAccent
+          color: Colors.yellowAccent),
+      keyboardType: TextInputType.visiblePassword,
+      obscureText: true,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.black12,
+        labelText: 'Senha',
+        //helperText: 'Senha de acesso do usuário',
+        border: new OutlineInputBorder(
+          //borderRadius: new BorderRadius.circular(25.0),
+          borderSide: new BorderSide(color: Colors.yellowAccent),
         ),
-        keyboardType: TextInputType.visiblePassword,
-        obscureText: true,
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: Colors.black12,
-          labelText: 'Senha',
-          //helperText: 'Senha de acesso do usuário',
-          border: new OutlineInputBorder(
-            //borderRadius: new BorderRadius.circular(25.0),
-            borderSide: new BorderSide(color: Colors.yellowAccent),
-          ),
-          icon: Icon(Icons.lock, color: Colors.yellowAccent),
-        ),
-        validator: (value){
-          if(value.isEmpty){
-            return 'Preencha a senha';
-          }
-          return null;
-        },
-      );
+        icon: Icon(Icons.lock, color: Colors.yellowAccent),
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Preencha a senha';
+        }
+        return null;
+      },
+    );
   }
 
-  Widget raisedButton_(){
-    return
-    RaisedButton(
-      onPressed: (){
-        if(_formKey.currentState.validate()){
+  Widget raisedButton_() {
+    return RaisedButton(
+      onPressed: () {
+        if (_formKey.currentState.validate()) {
           // Process data
           //print(_isOnline);
-          if(!_isOnline){
-            _showAlertDialog(context,'SEM CONEXÃO!');
-          }else {
-            _getUser(_login.text,_password.text).then((map){
-              if(map.isNotEmpty) {
-                if(map[0].status == 'OK'){
+          if (!_isOnline) {
+            _showAlertDialog(context, 'SEM CONEXÃO!');
+          } else {
+            _getUser(_login.text, _password.text).then((map) {
+              if (map.isNotEmpty) {
+                if (map[0].status == 'OK') {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
                           builder: (context) => MylistViewSoltAgendadas(
-                            nome: map[0].usuarnome,
-                            usuarcodigo: map[0].usuarcodigo,
-                          )
-                      )
-                  ).then((value) {
+                                nome: map[0].usuarnome,
+                                usuarcodigo: map[0].usuarcodigo,
+                              ))).then((value) {
                     _login.clear();
                     _password.clear();
                   });
-                }else{
-                  _showAlertDialog(context,'USUÁRIO OU SENHA INVÁLIDOS!');
+                } else {
+                  _showAlertDialog(context, 'USUÁRIO OU SENHA INVÁLIDOS!');
                   //print(map[0].query);
                 }
-              }else{
-                _showAlertDialog(context,'SEM DADOS!');
+              } else {
+                _showAlertDialog(context, 'SEM DADOS!');
                 print('Map veio vazio.');
                 print(map[0].query);
               }
             });
           }
-        }else{
-          _showAlertDialog(context,'PREENCHA TODOS OS CAMPOS!');
+        } else {
+          _showAlertDialog(context, 'PREENCHA TODOS OS CAMPOS!');
         }
       },
       child: Text('Entrar'),
@@ -254,13 +248,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Form(
-            key: _formKey,
-            child: container_()
-          )
-      );
+    return Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Form(key: _formKey, child: container_()));
   }
 }
